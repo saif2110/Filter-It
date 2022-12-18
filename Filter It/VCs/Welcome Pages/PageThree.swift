df3fc781d2911ab0734c158eee6b5f0deda2217b
Saif <saif.mukadam@we3.tech>
2022-12-18 13:00:40 +0530

    Done Minor Changes

--- Filter It/VCs/Welcome Pages/PageThree.swift
+++ Filter It/VCs/Welcome Pages/PageThree.swift
@@ -9,40 +9,37 @@ import UIKit
 import Lottie
 
 class PageThree: UIViewController {
+  
+  @IBOutlet weak var viewLottie: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet weak var viewLottie: UIImageView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let LottiV = AnimationView()
+    LottiV.frame = viewLottie.bounds
+    LottiV.backgroundColor = .clear
+    LottiV.animation = Animation.named("PageThree")
+    LottiV.contentMode = .scaleAspectFit
+    LottiV.loopMode = .loop
+    DispatchQueue.main.async {
+      self.viewLottie.addSubview(LottiV)
+      LottiV.play { bool in
         
-        let LottiV = AnimationView()
-        LottiV.frame = viewLottie.bounds
-        LottiV.backgroundColor = .clear
-        LottiV.animation = Animation.named("PageThree")
-        LottiV.contentMode = .scaleAspectFit
-        LottiV.loopMode = .loop
-        DispatchQueue.main.async {
-            self.viewLottie.addSubview(LottiV)
-            LottiV.play { bool in
-            
-            }
-            
-        }
-        
-        
+      }
     }
+  }
+  
+  @IBAction func next(_ sender: Any) {
+  
+    let generator = UIImpactFeedbackGenerator(style: .light)
+    generator.impactOccurred()
     
-    @IBAction func next(_ sender: Any) {
-        let generator = UIImpactFeedbackGenerator(style: .light)
-        generator.impactOccurred()
-        dismiss(animated: true) {
-            let vc = InAppPurchases()
-            vc.modalPresentationStyle = .fullScreen
-            if let topController = UIApplication.topViewController() {
-                topController.present(vc, animated: true, completion: nil)
-            }
-        }
+    dismiss(animated: true) {
+      let vc = InAppPurchases()
+      vc.modalPresentationStyle = .fullScreen
+      if let topController = UIApplication.topViewController() {
+        topController.present(vc, animated: true, completion: nil)
+      }
     }
-    
-    
+  }
 }
